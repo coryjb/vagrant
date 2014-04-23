b195964f46445e4b1458ea221652a52c80900aee
Cory Becker <cory@beckerwebsolutions.com>
2014-04-23 12:48:08 -0500

    Initial

---
+++ Vagrantfile
@@ -0,0 +1,28 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "precise64"
+
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 3306, host: 33066
+
+  config.vm.network "private_network", ip: "192.168.33.10"
+
+  # If true, then any SSH connections made will enable agent forwarding.
+  # Default value: false
+  # config.ssh.forward_agent = true
+
+  config.vm.synced_folder "./www", "/var/www/html"
+
+  config.vm.provider "virtualbox" do |vb|
+     vb.gui = false
+     vb.customize ["modifyvm", :id, "--memory", "512"]
+  end
+
+  config.vm.provision "shell", path: "scripts/provision.sh"
+end
---
+++ scripts/provision.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Global
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server mysql-server/root_password password root | debconf-set-selections
+echo mysql-server mysql-server/root_password_again password root | debconf-set-selections
+
+# Update system and install packages
+apt-get -y update
+apt-get -y install python-software-properties
+add-apt-repository ppa:ondrej/php5
+apt-get -y update
+apt-get -y -q install php5 apache2 mysql-server php5-mysql php5-json php5-curl git curl
+
+# Configure MySQL
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
+
+# Install phpMyAdmin
+cd /usr/local/src
+wget -O phpmyadmin.tgz "http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.1.13/phpMyAdmin-4.1.13-english.tar.gz?r=http%3A%2F%2Fwww.phpmyadmin.net%2Fhome_page%2Fdownloads.php"
+tar xzvf phpmyadmin.tgz
+mv phpMyAdmin-4.1.13-english /var/www/phpmyadmin
+
+# Configure Apache
+mkdir /var/www/html
+
+echo '<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+
+Alias /phpmyadmin "/var/www/phpmyadmin/"
+<Directory "/var/www/phpmyadmin/">
+    Order allow,deny
+    Allow from all
+</Directory>
+' > /etc/apache2/sites-available/000-default.conf
+
+a2enmod rewrite
+
+# Compass CSS compiler
+gem install compass
+
+# Composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/bin --filename=composer
+
+# Restart services
+service apache2 restart
+service mysql restart
